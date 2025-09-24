784f2200b2be9125a64d9d8a5e835b67675d082b
alpinnbtk <ambrosiusalvin20@gmail.com>
2025-09-24 22:57:01 +0700

    Revisi Project
    Project seng sebelum e gak iso di-run. Jadine pake copy project ndek kelas aowkwk.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'home',
+    pathMatch: 'full'
   },
   {
     path: 'login',
@@ -15,30 +20,32 @@ const routes: Routes = [
     loadChildren: () => import('./my-favorite/my-favorite.module').then( m => m.MyFavoritePageModule)
   },
   {
-    path: 'kategori',
-    loadChildren: () => import('./berita/kategori/kategori.module').then( m => m.KategoriPageModule)
+    path: 'berita',
+    children: [
+      {
+        path: 'baca',
+        loadChildren: () => import('./berita/baca/baca.module').then(m => m.BacaPageModule)
+      },
+      {
+        path: 'cari',
+        loadChildren: () => import('./berita/cari/cari.module').then(m => m.CariPageModule)
+      },
+      {
+        path: 'daftar',
+        loadChildren: () => import('./berita/daftar/daftar.module').then(m => m.DaftarPageModule)
+      },
+      {
+        path: 'kategori',
+        loadChildren: () => import('./berita/kategori/kategori.module').then(m => m.KategoriPageModule)
+      }
+    ]
   },
-  {
-    path: 'kategori',
-    loadChildren: () => import('./berita/kategori/kategori.module').then( m => m.KategoriPageModule)
-  },
-  {
-    path: 'daftar',
-    loadChildren: () => import('./berita/daftar/daftar.module').then( m => m.DaftarPageModule)
-  },
-  {
-    path: 'baca',
-    loadChildren: () => import('./berita/baca/baca.module').then( m => m.BacaPageModule)
-  },
-  {
-    path: 'cari',
-    loadChildren: () => import('./berita/cari/cari.module').then( m => m.CariPageModule)
-  }
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
